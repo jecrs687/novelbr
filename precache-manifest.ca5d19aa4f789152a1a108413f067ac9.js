58f807a578dd5c5c1d9521535cedc0157d8bc8f1
jecrs687 <emanuelcascone@gmail.com>
2021-02-23 13:51:32 -0300

    Updates

---
+++ precache-manifest.ca5d19aa4f789152a1a108413f067ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff24894f1941d0f2a84683ed103dadd9",
+    "url": "/novelBr/index.html"
+  },
+  {
+    "revision": "23105606622041c112dd",
+    "url": "/novelBr/static/css/main.932534de.chunk.css"
+  },
+  {
+    "revision": "2921343c104f6431c384",
+    "url": "/novelBr/static/js/2.b9b400dc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/novelBr/static/js/2.b9b400dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23105606622041c112dd",
+    "url": "/novelBr/static/js/main.646a6b98.chunk.js"
+  },
+  {
+    "revision": "e9e789b0940d52585039",
+    "url": "/novelBr/static/js/runtime-main.e1caf09c.js"
+  }
+]);
