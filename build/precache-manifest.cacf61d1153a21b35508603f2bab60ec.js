08bb243b7a1d88b39bdbda93881d8bdc512baa3c
José Emanuel <45917859+jecrs687@users.noreply.github.com>
2021-02-24 11:20:22 -0300

    Add files via upload

---
+++ build/precache-manifest.cacf61d1153a21b35508603f2bab60ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a4018e129cf1ec2b4bf74da968548e",
+    "url": "/novelbr/index.html"
+  },
+  {
+    "revision": "6b06733fca07b9bb9634",
+    "url": "/novelbr/static/css/main.932534de.chunk.css"
+  },
+  {
+    "revision": "1dad7cd26bdcd4c1dc0d",
+    "url": "/novelbr/static/js/2.85f40fa0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/novelbr/static/js/2.85f40fa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b06733fca07b9bb9634",
+    "url": "/novelbr/static/js/main.46cc7e41.chunk.js"
+  },
+  {
+    "revision": "7926a327424d1e0b3c95",
+    "url": "/novelbr/static/js/runtime-main.1e33d63d.js"
+  }
+]);
