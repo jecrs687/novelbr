b8aed3e3bddf32767da041d4a25c4eb292a71ae4
jecrs687 <emanuelcascone@gmail.com>
2021-03-08 08:25:03 -0300

    Updates

---
+++ precache-manifest.c77cd52a486902b1d39e5fb4d074c947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7675fb7edd07b8d6bf06b7d64cabb2fc",
+    "url": "/novelbr/index.html"
+  },
+  {
+    "revision": "5fc6cb82531c62bba402",
+    "url": "/novelbr/static/css/main.1f0a3222.chunk.css"
+  },
+  {
+    "revision": "27ef64cdf97569160031",
+    "url": "/novelbr/static/js/2.7269ca73.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/novelbr/static/js/2.7269ca73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc6cb82531c62bba402",
+    "url": "/novelbr/static/js/main.0faeb629.chunk.js"
+  },
+  {
+    "revision": "7926a327424d1e0b3c95",
+    "url": "/novelbr/static/js/runtime-main.1e33d63d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/novelbr/precache-manifest.d339ff1c982976e4f53ae0ec8c5999c3.js"
+  "/novelbr/precache-manifest.c77cd52a486902b1d39e5fb4d074c947.js"
 );
 
 self.addEventListener('message', (event) => {
